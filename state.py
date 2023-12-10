7219f91bd924eeb085b5d85be69225761e586887
Okyere Adubofuor <oksberg89@gmail.com>
2023-12-10 08:36:49 +0000

    deleted state.py

--- state.py
+++ state.py
@@ -1,18 +1,1 @@
-#!/usr/bin/python3
-"""Creates the class State."""
 
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """Inherits from the Basemodel and adds another
-    parameter called name
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Initializes the inherited attributes and
-        methods by calling super and __init__ methods
-        , and the name attribute too."""
-
-        super().__init__()
-        self.name = " "
